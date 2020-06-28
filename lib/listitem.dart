f5c85d5b335afcbef25cb669a1c4e806772a5d84
igor.zinin <igor.zinin@futuremark.com>
2020-06-29 00:08:55 +0300

    full - functional

--- lib/listitem.dart
+++ lib/listitem.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
 import 'package:radio_amoris/amoris_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'dart:convert';
 
-class Station extends StatefulWidget{
+class Station extends StatefulWidget {
   final String _descr;
   final String _baseUrl;
   final int _uid;
@@ -18,53 +20,63 @@ class Station extends StatefulWidget{
   }
 }
 
-class StationState extends State<Station>{
-  var _client = new http.Client();
+class StationState extends State<Station> {
+  IOClient _client;
   bool _allowhttp = true;
   String _lastsongtitle = '...';
 
+  @override
+  void initState() {
+    super.initState();
+    HttpClient client = new HttpClient()
+      ..badCertificateCallback =
+          ((X509Certificate cert, String host, int port) => true);
+    _client = new IOClient(client);
+  }
+
   Future<ChanInfo> _fetchChanInfo() async {
     final url = 'https://amoris.sknt.ru/sc/${widget._uid}/stats.json';
-    final response = await _client.get(url,
-      headers: {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36'
-      }
-    );
+    final response = await _client.get(url, headers: {
+      'user-agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36'
+    });
     _allowhttp = false;
     if (response.statusCode == 200) {
-      new Timer(const Duration(seconds: 30), ()=> _allowhttp = true);
+      new Timer(const Duration(seconds: 30), () => _allowhttp = true);
       return ChanInfo.fromJson(json.decode(utf8.decode(response.bodyBytes)));
     } else {
       throw Exception('Failed to load channel info');
     }
   }
-    
+
   @override
   Widget build(BuildContext context) {
     return new ScopedModelDescendant<AmorisModel>(
-      builder: (context, child, model) => new ListTile(  // sharedSel ----> FIXME! Text('${model.counter}'),
-        leading: model.getIgon(widget._uid),
-        title: Text(widget._descr),
-        subtitle: _allowhttp ? FutureBuilder<ChanInfo>(
-          future: _fetchChanInfo(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              _lastsongtitle = '${snapshot.data.songtitle} \nlisteners: ${snapshot.data.uniquelisteners}';
-              return Text(_lastsongtitle);
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-            // By default, show a loading spinner
-            return Text('...');
-          },
-        ) : Text(_lastsongtitle),
-        isThreeLine: true,
-        onTap: (){
-          model.select(widget._uid);
-        },
-        selected: model.isSelected(widget._uid)
-      )
-    );
+        builder: (context, child, model) => new ListTile(
+            // sharedSel ----> FIXME! Text('${model.counter}'),
+            leading: model.getIgon(widget._uid),
+            title: Text(widget._descr),
+            subtitle: _allowhttp
+                ? FutureBuilder<ChanInfo>(
+                    future: _fetchChanInfo(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        _lastsongtitle =
+                            '${snapshot.data.songtitle} \nlisteners: ${snapshot.data.uniquelisteners}';
+                        return Text(_lastsongtitle);
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
+                      }
+                      // By default, show a loading spinner
+                      return Text('...');
+                    },
+                  )
+                : Text(_lastsongtitle),
+            isThreeLine: true,
+            onTap: () {
+              model.select(widget._uid);
+            },
+            selected: model.isSelected(widget._uid)));
   }
 }
 
--- lib/stationsdata.dart
+++ lib/stationsdata.dart
@@ -1,70 +1,70 @@
 const StationsData = {
-   1:{
-      'url':'https://anima.sknt.ru/trance.mp3',
-      'descr':'"Trance" 160 MP3',
-   },
-   2:{
-      'url':'http://anima.sknt.ru/trance.aac',
-      'descr':'Anima Amoris [Trance] 80 AACP',
-   },
-   3:{
-      'url':'https://amoris.sknt.ru/dubtechno.mp3',
-      'descr':'"Dub Techno" 320 MP3',
-   },
-   4:{
-      'url':'https://amoris.sknt.ru/dubtechno.aac',
-      'descr':'"Dub Techno" 56 AACP',
-   },
-   15:{
-      'url':'https://amoris.sknt.ru/dubtechnomix.mp3',
-      'descr':'"Dub Techno Mix" 320 MP3',
-   },
-   5:{
-      'url':'https://amoris.sknt.ru/goa.mp3',
-      'descr':'"Goa Psy Trance" 160 MP3',
-   },
-   6:{
-      'url':'https://amoris.sknt.ru/dubstep.mp3',
-      'descr':'"Dub Step" 160 MP3',
-   },
-   7:{
-      'url':'https://amoris.sknt.ru/dnb.mp3',
-      'descr':'"Drum and Bass" 160 MP3',
-   },
-   8:{
-      'url':'https://amoris.sknt.ru/minimal.mp3',
-      'descr':'"Minimal Deep Techno" 160 MP3',
-   },
-   9:{
-      'url':'https://amoris.sknt.ru/deeptech.mp3',
-      'descr':'"Deep Tech House" 160 MP3',
-   },
-   10:{
-      'url':'https://amoris.sknt.ru/ambient.mp3',
-      'descr':'"Ambient" 160 MP3',
-   },
-   11:{
-      'url':'https://amoris.sknt.ru/idm.mp3',
-      'descr':'"IDM" 160 MP3',
-   },
-   12:{
-      'url':'https://amoris.sknt.ru/newage.mp3',
-      'descr':'"New Age" 160 MP3',
-   },
-   13:{
-      'url':'https://amoris.sknt.ru/triphop.mp3',
-      'descr':'"Trip Hop Lounge" 160 MP3',
-   },
-   14:{
-      'url':'https://amoris.sknt.ru/techno.mp3',
-      'descr':'"Techno" 160 MP3',
-   },
-   16:{
-      'url':'https://amoris.sknt.ru/electro.mp3',
-      'descr':'"Electro" 160 MP3',
-   },
-   17:{
-      'url':'https://amoris.sknt.ru/bible.mp3',
-      'descr':'"Библия (Bible in Russian)" 128 MP3',
-   },
+  1: {
+    'url': 'http://anima.sknt.ru/trance.mp3',
+    'descr': '"Trance" 160 MP3',
+  },
+  2: {
+    'url': 'http://anima.sknt.ru/trance.aac',
+    'descr': 'Anima Amoris [Trance] 80 AACP',
+  },
+  3: {
+    'url': 'http://amoris.sknt.ru/dubtechno.mp3',
+    'descr': '"Dub Techno" 320 MP3',
+  },
+  4: {
+    'url': 'http://amoris.sknt.ru/dubtechno.aac',
+    'descr': '"Dub Techno" 56 AACP',
+  },
+  15: {
+    'url': 'http://amoris.sknt.ru/dubtechnomix.mp3',
+    'descr': '"Dub Techno Mix" 320 MP3',
+  },
+  5: {
+    'url': 'http://amoris.sknt.ru/goa.mp3',
+    'descr': '"Goa Psy Trance" 160 MP3',
+  },
+  6: {
+    'url': 'http://amoris.sknt.ru/dubstep.mp3',
+    'descr': '"Dub Step" 160 MP3',
+  },
+  7: {
+    'url': 'http://amoris.sknt.ru/dnb.mp3',
+    'descr': '"Drum and Bass" 160 MP3',
+  },
+  8: {
+    'url': 'http://amoris.sknt.ru/minimal.mp3',
+    'descr': '"Minimal Deep Techno" 160 MP3',
+  },
+  9: {
+    'url': 'http://amoris.sknt.ru/deeptech.mp3',
+    'descr': '"Deep Tech House" 160 MP3',
+  },
+  10: {
+    'url': 'http://amoris.sknt.ru/ambient.mp3',
+    'descr': '"Ambient" 160 MP3',
+  },
+  11: {
+    'url': 'http://amoris.sknt.ru/idm.mp3',
+    'descr': '"IDM" 160 MP3',
+  },
+  12: {
+    'url': 'http://amoris.sknt.ru/newage.mp3',
+    'descr': '"New Age" 160 MP3',
+  },
+  13: {
+    'url': 'http://amoris.sknt.ru/triphop.mp3',
+    'descr': '"Trip Hop Lounge" 160 MP3',
+  },
+  14: {
+    'url': 'http://amoris.sknt.ru/techno.mp3',
+    'descr': '"Techno" 160 MP3',
+  },
+  16: {
+    'url': 'http://amoris.sknt.ru/electro.mp3',
+    'descr': '"Electro" 160 MP3',
+  },
+  17: {
+    'url': 'http://amoris.sknt.ru/bible.mp3',
+    'descr': '"Библия (Bible in Russian)" 128 MP3',
+  },
 };
