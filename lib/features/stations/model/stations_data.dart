72d1d39133747e99c01a52aad9b04b551eca2707
igor <igor.zinin@ul.com>
2024-04-02 18:24:28 +0300

    updating hardcoded stations data backup

--- lib/features/stations/model/stations_data.dart
+++ lib/features/stations/model/stations_data.dart
@@ -1,70 +1,119 @@
-const StationsData = {
-  1: {
-    'url': 'https://anima.sknt.ru/trance.mp3',
-    'descr': '"Trance" 160 MP3',
-  },
-  2: {
-    'url': 'https://anima.sknt.ru/trance.aac',
-    'descr': 'Anima Amoris [Trance] 80 AACP',
-  },
-  3: {
-    'url': 'https://amoris.sknt.ru/dubtechno.mp3',
-    'descr': '"Dub Techno" 320 MP3',
-  },
-  4: {
-    'url': 'https://amoris.sknt.ru/dubtechno.aac',
-    'descr': '"Dub Techno" 56 AACP',
-  },
-  15: {
-    'url': 'https://amoris.sknt.ru/dubtechnomix.mp3',
-    'descr': '"Dub Techno Mix" 320 MP3',
-  },
-  5: {
-    'url': 'https://amoris.sknt.ru/goa.mp3',
-    'descr': '"Goa Psy Trance" 160 MP3',
-  },
-  6: {
-    'url': 'https://amoris.sknt.ru/dubstep.mp3',
-    'descr': '"Dub Step" 160 MP3',
-  },
-  7: {
-    'url': 'https://amoris.sknt.ru/dnb.mp3',
-    'descr': '"Drum and Bass" 160 MP3',
-  },
-  8: {
-    'url': 'https://amoris.sknt.ru/minimal.mp3',
-    'descr': '"Minimal Deep Techno" 160 MP3',
-  },
-  9: {
-    'url': 'https://amoris.sknt.ru/deeptech.mp3',
-    'descr': '"Deep Tech House" 160 MP3',
-  },
-  10: {
-    'url': 'https://amoris.sknt.ru/ambient.mp3',
-    'descr': '"Ambient" 160 MP3',
-  },
-  11: {
-    'url': 'https://amoris.sknt.ru/idm.mp3',
-    'descr': '"IDM" 160 MP3',
-  },
-  12: {
-    'url': 'https://amoris.sknt.ru/newage.mp3',
-    'descr': '"New Age" 160 MP3',
-  },
-  13: {
-    'url': 'https://amoris.sknt.ru/triphop.mp3',
-    'descr': '"Trip Hop Lounge" 160 MP3',
-  },
-  14: {
-    'url': 'https://amoris.sknt.ru/techno.mp3',
-    'descr': '"Techno" 160 MP3',
-  },
-  16: {
-    'url': 'https://amoris.sknt.ru/electro.mp3',
-    'descr': '"Electro" 160 MP3',
-  },
-  17: {
-    'url': 'https://amoris.sknt.ru/bible.mp3',
-    'descr': '"Библия (Bible in Russian)" 128 MP3',
-  },
-};
+{
+	"channels": [
+		{
+			"name": "Trance",
+			"listenurl": "https://amoris.sknt.ru/trance",
+			"metadata": "https://anima.sknt.ru/trance/stats.json"
+		},
+		{
+			"name": "Dub Techno",
+			"listenurl": "https://amoris.sknt.ru/dubtechno",
+			"metadata": "https://amoris.sknt.ru/dubtechno/stats.json"
+		},
+		{
+			"name": "Electronic Styles",
+			"listenurl": "https://amoris.sknt.ru/electronic",
+			"metadata": "https://amoris.sknt.ru/electronic/stats.json"
+		},
+		{
+			"name": "Goa Psy Trance",
+			"listenurl": "https://amoris.sknt.ru/goa",
+			"metadata": "https://amoris.sknt.ru/goa/stats.json"
+		},
+		{
+			"name": "Dub Step",
+			"listenurl": "https://amoris.sknt.ru/dubstep",
+			"metadata": "https://amoris.sknt.ru/dubstep/stats.json"
+		},
+		{
+			"name": "Drum and Bass",
+			"listenurl": "https://amoris.sknt.ru/dnb",
+			"metadata": "https://amoris.sknt.ru/dnb/stats.json"
+		},
+		{
+			"name": "Minimal Deep Techno",
+			"listenurl": "https://amoris.sknt.ru/minimal",
+			"metadata": "https://amoris.sknt.ru/minimal/stats.json"
+		},
+		{
+			"name": "Deep Tech House",
+			"listenurl": "https://amoris.sknt.ru/deeptech",
+			"metadata": "https://amoris.sknt.ru/deeptech/stats.json"
+		},
+		{
+			"name": "Ambient",
+			"listenurl": "https://amoris.sknt.ru/ambient",
+			"metadata": "https://amoris.sknt.ru/ambient/stats.json"
+		},
+		{
+			"name": "IDM",
+			"listenurl": "https://amoris.sknt.ru/idm",
+			"metadata": "https://amoris.sknt.ru/idm/stats.json"
+		},
+		{
+			"name": "New Age",
+			"listenurl": "https://amoris.sknt.ru/newage",
+			"metadata": "https://amoris.sknt.ru/newage/stats.json"
+		},
+		{
+			"name": "Trip Hop Lounge",
+			"listenurl": "https://amoris.sknt.ru/triphop",
+			"metadata": "https://amoris.sknt.ru/triphop/stats.json"
+		},
+		{
+			"name": "Techno",
+			"listenurl": "https://amoris.sknt.ru/techno",
+			"metadata": "https://amoris.sknt.ru/techno/stats.json"
+		},
+		{
+			"name": "Dub Techno Mix",
+			"listenurl": "https://amoris.sknt.ru/dubtechnomix",
+			"metadata": "https://amoris.sknt.ru/dubtechnomix/stats.json"
+		},
+		{
+			"name": "Electro",
+			"listenurl": "https://amoris.sknt.ru/electro",
+			"metadata": "https://amoris.sknt.ru/electro/stats.json"
+		},
+		{
+			"name": "Bass",
+			"listenurl": "https://amoris.sknt.ru/bass",
+			"metadata": "https://amoris.sknt.ru/bass/stats.json"
+		},
+		{
+			"name": "Breakbeat Hardcore",
+			"listenurl": "https://amoris.sknt.ru/breakbeat",
+			"metadata": "https://amoris.sknt.ru/breakbeat/stats.json"
+		},
+		{
+			"name": "Breaks",
+			"listenurl": "https://amoris.sknt.ru/breaks",
+			"metadata": "https://amoris.sknt.ru/breaks/stats.json"
+		},
+		{
+			"name": "IDM Break Core",
+			"listenurl": "https://amoris.sknt.ru/breakcore",
+			"metadata": "https://amoris.sknt.ru/breakcore/stats.json"
+		},
+		{
+			"name": "Dub",
+			"listenurl": "https://amoris.sknt.ru/dub",
+			"metadata": "https://amoris.sknt.ru/dub/stats.json"
+		},
+		{
+			"name": "Reggae",
+			"listenurl": "https://amoris.sknt.ru/reggae",
+			"metadata": "https://amoris.sknt.ru/reggae/stats.json"
+		},
+		{
+			"name": "DJ Pepe Collection",
+			"listenurl": "https://amoris.sknt.ru/djpepe",
+			"metadata": "https://amoris.sknt.ru/djpepe/stats.json"
+		},
+		{
+			"name": "Bible",
+			"listenurl": "https://amoris.sknt.ru/bible",
+			"metadata": "https://amoris.sknt.ru/bible/stats.json"
+		}
+	]
+}
